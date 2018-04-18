3a89e3ebd8df1fe3a605e1e72d06c0a0d525a5d7
Josefine <josefine@nocweb.se>
2018-04-18 16:09:18 +0200

    added privacy policy

---
+++ privacy_policy.php
@@ -0,0 +1,30 @@
+<?php $title = "Privacy Policy"; include('header.php'); ?>
+
+
+  <div class="imagewrapper--small wood">
+    <div class="header-image image"></div>
+    <h1>Privacy Policy</h1>
+  </div><div class="menu">
+      <ul class="container">
+        <li class="h3 title">MENY</li>
+        <li class="h3"><a href="index.php">HEM</a></li>
+        <li class="h3">FÅR</li>
+        <li class="h3"><a href="honor.php">HÖNS</a></li>
+        <li class="h3"><a href="halmhus.php">HALMHUS</a></li>
+        <li class="h3"><a href="sakersomhander.php">SAKER SOM HÄNDER</a></li>
+        <li class="h3"><a href="folkochfa.php">FOLK & FÄ</a></li>
+        <li class="h3"><a href="#footer">KONTAKT</a></li>
+      </ul>
+  </div>
+
+  <div class="section container">
+    <h2>Privacy Policy</h2>
+    <div class="col--12 ">
+      <h3>WE CANNOT SEE ANY DATA OR MESSAGES YOU SEND TO YOUR CONTACTS.  PERIOD. </h3>
+
+<p>We don't store your data and it is a promise.</p>
+    </div> 
+  
+  </div>
+
+<?php include('footer.php'); ?>
