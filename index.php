bdd8a459b0e068741cc0bcd06767ce528ae84695
Josefine <josefine@nocweb.se>
2018-11-14 21:33:41 +0100

    uppdaterad payoff

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </div>
   <?php include('menu.php'); ?>
 
-  <div class="payoff"><p class="container">Verkligheten är en familjegård där vi lever, bor och samtidigt bygger upp en liten verksamhet med fokus på småskalighet, äkthet och välkomnande.</p></div>
+  <div class="payoff"><p class="container">Verkligheten är en familjegård där vi lever, bor och samtidigt bygger upp en liten verksamhet där allt sker i liten skala och med omtanke om människor och miljö.</p></div>
   
   <div class="section">
     <div class="container center-align">
