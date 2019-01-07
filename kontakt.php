16a1533dc331d07f0299eeaf62a3cb8a99df1c9b
Josefine <josefine@nocweb.se>
2019-01-07 22:23:04 +0100

    mailform added och ändrad mail

--- kontakt.php
+++ kontakt.php
@@ -11,6 +11,29 @@
 
   <div class="col--12">
     
+    <h3>NYHETSBREV</h3>
+    <p>Skriv upp dig här nedan om du vill få inbjudan till våra marknader eller annan information som rör verksamheten här på gården. </p>
+    <!-- Begin Mailchimp Signup Form -->
+    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+      #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+      /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+         We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    </style>
+    <div id="mc_embed_signup">
+    <form action="https://verklighetensgard.us7.list-manage.com/subscribe/post?u=083dd14279b927df3be16965d&amp;id=47495c5a79" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+      
+      <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_083dd14279b927df3be16965d_47495c5a79" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Skicka" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+    </form>
+    </div>
+
+    <!--End mc_embed_signup-->
+
     <h3>FACEBOOK & INSTAGRAM</h3>
     <ul class="left-align">
       <li>
@@ -34,7 +57,7 @@
   </div>
 
   <div class="col--4">
-    <a class="button" href="mailto:verklighetensgard@gmail.com"><span class="icon icon--mail-white mr"></span>Maila oss!</a>
+    <a class="button" href="mailto:info@verklighetensgard.se"><span class="icon icon--mail-white mr"></span>Maila oss!</a>
   </div>
 
   <div class="col--12">
