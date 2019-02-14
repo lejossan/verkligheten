9a2b2baaa3fb4a71b7a7f1ec2e3b69b484700515
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-02-14 22:07:29 +0100

    ändrade lite text på camping

--- camping.php
+++ camping.php
@@ -12,13 +12,13 @@
     <div class="col--12">
       <h2 class="mt">CAMPING I VERKLIGHETEN</h2>
       <p>
-				Nu har du möjlighet att stanna en natt eller fler i Verkligheten! Till sommaren har vi öppnat en minicamping med tre ställplatser för husbil eller husvagn och även plats för tält. Här sover du på det vi kallar midsommarängen precis utanför hagen. Tillgång till vattentoalett och dusch finns i det gamla utedasset, här finns även en enklare ho till disken.
+				Nu har du möjlighet att stanna en natt eller fler i Verkligheten! Till sommaren öppnar vi en minicamping med tre ställplatser för husbil eller husvagn och även plats för tält. Här sover du på det vi kallar midsommarängen precis utanför hagen. Tillgång till vattentoalett och dusch finns i det gamla utedasset, här finns även en enklare ho till disken.
 			</p>
 			<p>
 				Här prioriterar vi närhet till naturen och enkelheten. Kanske hälsar fåren på under morgonkaffet eller kanske har gårdens höns värpt så det finns färska ägg att köpa? På sommaren finns nästan alltid möjlighet att skörda grönsaker till kvällens middag, eller kanske hyra grillplatsen uppe i skogen? 
 			</p>
 			<p>
-				Vill du som sover i tält hellre slå upp tältet i skogen än på ängen går det bra,  här kan du tälta i skogen samtidigt som du har tillgång till toalett och dusch! 
+				Vill du som sover i tält hellre slå upp tältet i skogen än på ängen går det bra, här kan du tälta i skogen samtidigt som du har tillgång till toalett och dusch! 
 			</p>
 			<p>
 				Håll utkik på hemsidan för öppettider, vi tar inga förbokningar men det går bra att maila innan för att se så det finns plats. 
@@ -39,10 +39,10 @@
       <h3>AKTIVITETER I NÄRHETEN</h3>
       <p>Besökstips från oss är Trollegater och Hackelboö naturreservat, det är våra favoritmål när vi ska göra en kortare utflykt med barnen. </p>
       <h4>Trollegater</h4>
-      <p>Gör en utflykt till en av sveriges största urbergsgrottor, här finns även en grillplats och en sjukt stor gran. Läs mer hos <a href="https://www.kindaturism.se/v?plats_id=1562">Kinda Turism</a>. </p>
+      <p>Gör en utflykt till en av sveriges största urbergsgrottor, här finns även en grillplats och en sjukt stor gran. Läs mer hos <a href="https://www.kindaturism.se/v?plats_id=1562" target="_blank">Kinda Turism</a>. </p>
 
       <h4>Hackelboö</h4>
-      <p>Är ett trivsamt ordnat naturreservat längs med riksväg 34, här finns fem spännande uppdrag för barn och även en liten badstrand och grillplats. Läs mer hos <a href="https://www.lansstyrelsen.se/ostergotland/besok-och-upptack/naturreservat/hackelboon-naturreservat.html">Länsstyrelsen</a>.</p>
+      <p>Är ett trivsamt ordnat naturreservat längs med riksväg 34, här finns fem spännande uppdrag för barn och även en liten badstrand och grillplats. Läs mer hos <a href="https://www.lansstyrelsen.se/ostergotland/besok-och-upptack/naturreservat/hackelboon-naturreservat.html" target="_blank">Länsstyrelsen</a>.</p>
 
       <h4>Badplatser</h4>
       <p>Flera badplatser finns i närheten men den som vi brukar åka till är Hultabadet i Järnlunden som ligger ca 5 km bort. Där finns både omklädningsrum, toa och en liten glasskiosk. </p>
