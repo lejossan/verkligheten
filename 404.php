7a3888532d78432cc125986b93809864e7ba657c
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-02-14 22:02:02 +0100

    added 404

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php $title = "Camping"; include('header.php'); ?>
+
+  <div class="imagewrapper--small wood">
+    <div class="header-image image"></div>
+    <h1>404</h1>
+  </div>
+  <?php include('menu.php'); ?>
+
+<div class="section container">
+    <div class="col--12">
+      <h2 class="mt mb">Du har gått vilse!</h2>
+			<p>Den här sidan finns inte. Kolla in menyn eller skicka oss ett mail och säg vad du letade efter.</p>
+      <div class="col--6 mb mt"><a href="mailto:info@verklighetensgard.se" class="button"><span class="icon icon--mail-white mr"></span><span>Maila oss!</span></a></div>
+	</div>
+</div>
+<?php include('footer.php'); ?>
