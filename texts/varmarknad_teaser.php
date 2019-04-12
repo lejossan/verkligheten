2254fa05a9b2345fe46e411faa802c12874df24b
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-04-12 19:29:23 +0200

    tider för vårmarknad

--- texts/varmarknad_teaser.php
+++ texts/varmarknad_teaser.php
@@ -4,7 +4,7 @@ echo '<h2>Vår i Verkligheten</h2>';
 
 echo '<p>Vårmarknad här i Verkligheten bjuder på djurmys, vårkänslor, fika, försäljning av stickat, virkat, sytt och syltat. Håll utkik på vår facebook sida för mer information!</p>';
 
-echo '<p class="strong">Nästa vårmarknad äger rum 12 maj 2019!</p>';
+echo '<p class="strong">Nästa vårmarknad äger rum 12 maj 2019 13:00 - 17:00!</p>';
 
 echo '<a class="readmore" href="aktuellt.php">Läs mer om Vårmarknaden här</a>';
 
