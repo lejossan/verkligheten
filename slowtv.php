8c0b0688fcb6083242b18d6b368893bfdc0b870f
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-04-22 18:21:08 +0200

    slowtv som youtube

--- slowtv.php
+++ slowtv.php
@@ -2,6 +2,7 @@
 
 
   <?php include('menu.php'); ?>
-<iframe class="slowtv" src="https://blurg.se"></iframe>
+
+<iframe class="slowtv" width="2179" height="924" src="https://www.youtube.com/embed/yxbdfTKmTKk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 <?php include('footer.php'); ?>
