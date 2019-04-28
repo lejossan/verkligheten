9a189538b83616b849c8249b8325fb1e4e19cee9
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-04-28 09:05:14 +0200

    ny länk till slowtv

--- slowtv.php
+++ slowtv.php
@@ -3,6 +3,6 @@
 
   <?php include('menu.php'); ?>
 
-<iframe class="slowtv" width="2179" height="924" src="https://www.youtube.com/embed/NfnXlOUtOi4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe class="slowtv" width="2179" height="924" src="https://www.youtube.com/embed/DJ776i5FZ8w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 <?php include('footer.php'); ?>
