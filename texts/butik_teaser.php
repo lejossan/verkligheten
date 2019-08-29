aef422d024e6e58a394a465b447714228395823a
Josefine Espenkrona <josefine.espenkrona@sportsediting.se>
2019-08-29 20:24:27 +0200

    uppdaterade butikteaser

--- texts/butik_teaser.php
+++ texts/butik_teaser.php
@@ -2,7 +2,7 @@
 
 echo '<h2 class="mt" style="margin-top:50px;">Besök vår sommarbutik!</h2>';
 
-echo '<p>Här hittar du säsongens grönsaker, tvålar, textilier och massa annat. Nu i augusti har vi öppet endast lördagar 11-17. Sista helgen vi har öppet för den här säsongen är i samband med Östgötadagarna 7-8 september. Välkomna!</p>';
+echo '<p>Här hittar du säsongens grönsaker, tvålar, textilier och massa annat. Sista helgen vi har öppet för den här säsongen är i samband med Östgötadagarna 7-8 september. De sista helgerna vi har kvar har vi fina erbjudande och utförsäljning på delar av sortimentet! Dessutom är det 50 % på ALLT i loppisen. Hoppas vi ses! Välkomna lördag och söndag mellan 11-17. </p>';
 
 echo '<a class="readmore" href="kontakt.php">Hitta hit</a>';
 echo '<br/>';
