5b3dd5a563bddec89230e46f8b4927d3935703d7
Josse <>
2021-05-01 09:25:51 +0200

    ändrade butiksöppettider

--- texts/butik_teaser.php
+++ texts/butik_teaser.php
@@ -1,17 +1,11 @@
-<!--<?php
 
-echo '<h2 class="mt" style="margin-top:50px;">Säsongsöppning!</h2>';
-
-echo '<p><b>21-24 maj öppnar vi för säsongen!</b> <br/>Här finns glass, kaffe och saft. Lammen är i hagen bredvid och kaninerna finns att klappa. Vi har möblerat om lite och ber alla ta hänsyns till varandra och vänta utanför om det blir trångt.</p>';
-
-?>-->
 
 
 <?php
 
 echo '<h2 class="mt" style="margin-top:50px;">Besök vår gårdsbutik!</h2>';
 
-echo '<p>Här hittar du säsongens grönsaker, tvålar, textilier och massa annat. Du kan även ta en glass och passa på att mata kaninerna. Hoppas vi ses! Välkomna lördagar mellan 11-17. </p>';
+echo '<p>Här hittar du prylar för utflykten, tvålar, filtar och massa annat. Du kan även ta en glass och passa på att mata kaninerna. Hoppas vi ses! Välkomna lördagar mellan 11-17. </p>';
 //echo '<p>Här hittar du säsongens grönsaker, tvålar, textilier och massa annat. Sista helgen vi har öppet för den här säsongen är i samband med Östgötadagarna 7-8 september. De sista helgerna har vi fina erbjudanden och utförsäljning på delar av sortimentet! Dessutom är det 50 % på ALLT i loppisen. Hoppas vi ses! Välkomna lördag och söndag mellan 11-17. </p>';
 
 //echo '<a class="readmore" href="kontakt.php">Hitta hit</a>';
