eae236ac35c58cd2e49b9d04f4f4c7047c33efb7
Josse <>
2021-07-16 13:22:26 +0200

    handelsbod text

--- handelsbod.php
+++ handelsbod.php
@@ -10,15 +10,14 @@
   <div class="section container">
     <div class="col--12">
       <h2>MED OMTANKE OM MÄNNISKOR OCH MILJÖ</h2>
-      <p>Här hittar du allt från tvål, textilier till dukning och bad, köksredskap till glass och prylar till utflykten och grillen. Vi väljer våra produkter med omsorg och vårt fokus ligger på att välja produkter som håller länge och där både tillverkning och användande sker på ett miljösmart sätt. </p>
-      <p>När du handlar i butiken kan du vara säker på att vilken sak du än fastnar för så är den tillverkad med omsorg om människor och miljö! </p>
+      <p>Här hittar du allt från friluftsprylar som kåsor, sporkar till tvål, spel, köksredskap och glass. Vi väljer våra produkter med omsorg och vårt fokus ligger på att välja produkter som håller länge och där både tillverkning och användande sker på ett miljösmart sätt. </p>
       <p>Ibland ordnar vi även marknader, då bjuder vi in lite fler försäljare och hantverkare. Då har vi även fikaservering och möjlighet att hälsa på gårdens djur. </p>
       <br/>
       <p class="col--6 mb">Betalning sker med kort, kontant eller Swisch.</p>
     <div>
       <h3 class="mt">ÖPPETTIDER</h3>
       <ul class="col--6 listbox mb">
-        <li><span class="mr">Lördagar</span> <span>11-17</span></li>
+        <li><span class="mr">Lördagar</span> <span>10:30-17</span></li>
       </ul>
         
       </div>
